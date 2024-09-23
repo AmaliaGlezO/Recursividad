0dac48eaa014aed957ee698bc3f4628fe6d88f06
AmaliaGlezO <amalia.glezo@gmail.com>
2024-09-23 08:55:42 -0600

    .

---
+++ python.py
@@ -0,0 +1,14 @@
+nombre = "Amalia"
+edad = 20
+altura = 1.60
+nota = 92
+
+# CONDICIONALES
+if nota >= 90:
+    print("Sobresaliente")
+elif nota >= 75:
+    print("Bien")
+elif nota >= 60: 
+    print("Suficiente")
+else:
+    print("Insuficiente")
