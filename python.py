daf33ef98808e41b0c123bdc67c8dc1fc29e430d
AmaliaGlezO <amalia.glezo@gmail.com>
2024-09-23 09:36:06 -0600

    .

--- python.py
+++ python.py
@@ -12,3 +12,17 @@ elif nota >= 60:
     print("Suficiente")
 else:
     print("Insuficiente")
+# Acabamos de ver que el if no le importa ser llamado. El trabaja porque está aquí me toca y paso
+
+# BUCLES
+notas = [['Amalia','juan','luis','Yandri','Ana'],[92,34,76,68,100]]
+
+for nota in notas[1]:
+    if nota >= 90:
+        print(notas[0][0],"Sobresaliente")
+    elif nota >= 75:
+        print(notas[0][1],"Bien")
+    elif nota >= 60: 
+        print(notas[0][2],"Suficiente")
+    else:
+        print(notas[0][3],"Insuficiente")
